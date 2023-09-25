8e14f9b6ea7b7276c00154c399b8f047984817a4
YBoah <yawboahene.yb@gmail.com>
2023-09-25 22:54:16 +0000

    Created const variables to contain the pswd values

---
+++ script.js
@@ -0,0 +1,9 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+ 
